62ad763a1c5980d8e03b4ff9a773d82964a508bd
loginov <pandamiksms@gmail.com>
2026-03-25 23:13:43 +0300

    Modification3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketAmount = 10000;
+        int ticketAmount = 10_000;
         int oneBonusMile = 20;
         int bonus = ticketAmount / oneBonusMile;
         System.out.println(" Количество начисляемых миль за купленный билет: " + bonus);
